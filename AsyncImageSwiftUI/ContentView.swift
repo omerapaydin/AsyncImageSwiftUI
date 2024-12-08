fdad706f4bb4ff36ba2b81a19ddd94d02321548f
Ömer Apaydın <omerapaydin90@gmail.com>
2024-12-08 16:54:50 +0300

    Second

--- AsyncImageSwiftUI/ContentView.swift
+++ AsyncImageSwiftUI/ContentView.swift
@@ -9,15 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationView {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+                                       
+            AsyncImage(url: URL(string: "https://atilsamancioglu.com/wp-content/uploads/2018/01/photo2.jpg")!) { image in
+                    image.resizable().frame(width: 300, height: 275, alignment: .center)
+                    } placeholder: {
+                           ProgressView()
+                    }
+                        
+                       
+                       
+                List(superHeroArray) { superhero in
+                           Text(superhero.name)
+                               .font(.title3)
+                               .foregroundColor(.blue)
+                       }
+                   }.navigationTitle(Text("Superhero Book"))
+                       
+                            
+               }
+
+               }
     }
-}
+
 
 #Preview {
     ContentView()
