7d90fd48cd8c14acc1a7680b5ad42aeec450c1eb
Ömer Apaydın <omerapaydin90@gmail.com>
2024-12-08 16:48:25 +0300

    Initial Commit

---
+++ AsyncImageSwiftUI/AsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUIApp.swift
+//  AsyncImageSwiftUI
+//
+//  Created by Ömer on 8.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImageSwiftUITests/AsyncImageSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUITests.swift
+//  AsyncImageSwiftUITests
+//
+//  Created by Ömer on 8.12.2024.
+//
+
+import Testing
+@testable import AsyncImageSwiftUI
+
+struct AsyncImageSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
